e09a965b9107d0b464fb504b73839d03a20e7550
fla <flaflutsch@mail.de>
2024-01-07 03:39:09 +0000

    Refactor python script

--- asus_fliplock.py
+++ asus_fliplock.py
@@ -6,21 +6,22 @@ import os
 import re
 import sys
 from typing import Optional
-from libevdev import Device, EV_SW
+
+from libevdev import EV_SW, Device
 
 # Setup logging
 # LOG=DEBUG sudo -E ./asus_fliplock.py "default" # all messages
 # LOG=ERROR sudo -E ./asus_fliplock.py "default" # only error messages
 logging.basicConfig()
-log = logging.getLogger('Asus fliplock')
-log.setLevel(os.environ.get('LOG', 'INFO'))
+log = logging.getLogger("Asus fliplock")
+log.setLevel(os.environ.get("LOG", "INFO"))
 
 # Layout
-layout = 'default'
+layout = "default"
 if len(sys.argv) > 1:
     layout = sys.argv[1]
 
-fliplock_layouts = importlib.import_module('conf.'+ layout)
+fliplock_layouts = importlib.import_module("conf." + layout)
 
 switches: Optional[str] = None
 wmi_hotkeys: Optional[str] = None
@@ -28,8 +29,8 @@ wmi_hotkeys: Optional[str] = None
 switches_detected = 0
 wmi_hotkeys_detected = 0
 
-def search_devices():
 
+def search_devices():
     global switches, wmi_hotkeys
     global touchpad_detected, switches_detected, wmi_hotkeys_detected
 
@@ -37,27 +38,27 @@ def search_devices():
 
     # Look into the devices file
     while tries > 0:
-
-        with open('/proc/bus/input/devices', 'r') as f:
+        with open("/proc/bus/input/devices", "r") as f:
             lines = f.readlines()
             for line in lines:
-                    
                 # Look for the device switches
                 if re.search("switches", line):
                     switches_detected = 1
-                    log.debug('Detect switches from %s', line.strip())
+                    log.debug("Detect switches from %s", line.strip())
 
                 # Look for the device wmi hotkeys
-                if wmi_hotkeys_detected == 0 and ("Name=\"Asus WMI hotkeys" in line):
+                if wmi_hotkeys_detected == 0 and ('Name="Asus WMI hotkeys' in line):
                     wmi_hotkeys_detected = 1
-                    log.debug('Detect wmi hotkeys from %s', line.strip())
+                    log.debug("Detect wmi hotkeys from %s", line.strip())
 
                 if wmi_hotkeys_detected == 1:
                     if "H: " in line:
                         wmi_hotkeys = line.split("event")[1]
                         wmi_hotkeys = wmi_hotkeys.split(" ")[0]
                         wmi_hotkeys_detected = 2
-                        log.debug('Set wmi hotkeys id %s from %s', wmi_hotkeys, line.strip())
+                        log.debug(
+                            "Set wmi hotkeys id %s from %s", wmi_hotkeys, line.strip()
+                        )
                         break
 
                 if switches_detected == 1:
@@ -65,8 +66,7 @@ def search_devices():
                         switches = line.split("event")[1]
                         switches = switches.split(" ")[0]
                         switches_detected = 2
-                        log.debug('Set switches id %s from %s', switches, line.strip())
-                        break
+                        log.debug("Set switches id %s from %s", switches, line.strip())
 
                 if switches_detected == 2 and wmi_hotkeys_detected == 2:
                     break
@@ -80,17 +80,19 @@ def search_devices():
         else:
             break
 
+
 search_devices()
 
 # Start monitoring the switch device
 fd_t_switches = None
 d_t_switches = None
 
-fd_t_wmi_hotkeys = open('/dev/input/event' + str(wmi_hotkeys), 'rb')
+fd_t_wmi_hotkeys = open("/dev/input/event" + str(wmi_hotkeys), "rb")
 d_t_wmi_hotkeys = Device(fd_t_wmi_hotkeys)
 
 # When we listen only key we predict start state is laptop mode
 intel_hid_switches_tablet_mode = 1
+
 
 def execute_cmd(cmd):
     try:
@@ -104,24 +106,22 @@ def execute_cmds_in_array(cmds):
         execute_cmd(cmd)
 
 
-def flip(intel_hid_switches_tablet_mode):    
+def flip(intel_hid_switches_tablet_mode):
     if intel_hid_switches_tablet_mode:
         execute_cmds_in_array(fliplock_layouts.tablet_mode_actions)
     else:
         execute_cmds_in_array(fliplock_layouts.laptop_mode_actions)
 
 
-# If mode has been changed, do something        
+# If mode has been changed, do something
 if switches is None and wmi_hotkeys is not None:
-
     for e in d_t_wmi_hotkeys.events():
-
         log.debug(e)
 
         if switches is not None:
             break
 
-        # Pressed and released event is called during one! flipping simultanously
+        # Pressed and released event is called during one! flipping simultaneously
         if e.matches(fliplock_layouts.flip_key) and e.value == 1:
             flip(intel_hid_switches_tablet_mode)
             intel_hid_switches_tablet_mode = not intel_hid_switches_tablet_mode
@@ -130,13 +130,12 @@ if switches is None and wmi_hotkeys is not None:
 
 
 if switches is not None:
-
-    fd_t_switches = open('/dev/input/event' + str(switches), 'rb')
+    fd_t_switches = open("/dev/input/event" + str(switches), "rb")
     d_t_switches = Device(fd_t_switches)
 
     for e in d_t_switches.events():
-
         log.debug(e)
 
         if e.matches(EV_SW.SW_TABLET_MODE):
             flip(e.value)
+
